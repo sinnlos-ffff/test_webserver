d3a18ad8d1d44ae8b878eb3bda3e632a7ec77958
Doyoung Kim <lark.noanswer@gmail.com>
2024-11-11 00:04:24 +0900

    Add struct for Message

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import "net"
 import "fmt"
 import "log"
 
+type Message struct {
+	source string
+	payload []byte
+}
+
 type Server struct {
 	address string
 	listener net.Listener
 	quitChannel     chan struct{}
+	messageChannel chan Message
 }
 
 func NewServer(address string) *Server {
 	return &Server{
 		address:    address,
-		quitChannel: make(chan struct{}),
+		quitChannel:    make(chan struct{}),
+		// without buffer, the channel will block the readLoop
+		messageChannel: make(chan Message, 10),
 	}
 }
 
@@ -42,8 +50,10 @@ func (server *Server) readLoop(conn net.Conn) {
 			continue
 		}
 
-		message := buf[:n]
-		fmt.Println(string(message))
+		server.messageChannel <- Message{
+			source: conn.RemoteAddr().String(),
+			payload: buf[:n],
+		}
 	}
 }
 
@@ -60,11 +70,19 @@ func (server *Server) Start() error {
 
 	// blocks return until quitChannel is closed
 	<-server.quitChannel
+	close(server.messageChannel)
 
 	return nil
 }
 
 func main() {
 	server := NewServer(":8000")
+
+	go func() {
+		for message := range server.messageChannel {
+			fmt.Printf("Received message from %s: %s\n", message.source, string(message.payload))
+		}
+	}()
+
 	log.Fatal(server.Start())
 }
